d061380e200ceb7b14ce6a52c122e5bc9012fa31
sgc6512 <81432815+sgc6512@users.noreply.github.com>
2023-03-13 17:32:13 +0000

    Initial commit

---
+++ Battleship.Test/ExampleTest.cs
@@ -0,0 +1,13 @@
+namespace Battleship.Test
+{
+    public class ExampleTest
+    {
+        [Fact]
+        public void TestPlay()
+        {
+            var ships = new[] { "3:2,3:5" };
+            var guesses = new[] { "7:0", "3:3" };
+            Game.Play(ships, guesses).Should().Be(0);
+        }
+    }
+}
